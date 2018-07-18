2869cd734c415bed254b0e7b01bfd6fd6655cc45
Justin Wang <justinw827@aol.com>
2018-07-18 00:49:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,8 @@ def prime?(num)
     return false
   else
     a = rand(2...num)
+    check1 = ((a ** (num-1)) / 2) % num - 1
+    check2 = ((a ** (num-1)) / 2) % num + 1
     if ((a ** (num-1)) / 2) % num == 0
       return true
     else
